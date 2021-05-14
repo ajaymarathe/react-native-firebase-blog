4975a5635c6f68ef28348017014ce9e945339b77
Ajay Marathe <ajay@learny.co>
2021-05-14 08:43:36 +0530

    setup firebase for blog app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+export class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text> hello world </Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default App;
